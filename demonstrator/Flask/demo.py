5e7d59a60bd090d67c99043a742664020f6c9bdb
Severin Simmler <severin.simmler@yahoo.com>
2016-08-22 13:37:32 +0200

    Update demo.py

--- demonstrator/Flask/demo.py
+++ demonstrator/Flask/demo.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('minimal.html')
 
 @app.route('/upload', methods=['POST'])
 def upload_file():
